d674225cfcd2cd409e23e729097b1c7cbbf58160
Sachin Patel <sachpatel1220@gmail.com>
2021-10-09 00:18:06 -0500

    settings.py fixes

--- goal_tree/goal_tree/settings.py
+++ goal_tree/goal_tree/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-365=v1bpm2d-4)wmfbic_s+yf2sjnz!er9co*lh11sdmtvj!+@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 

b8afcb8b4ac971e297f8bc38ef39c08afded92f9
Sachin Patel <sachpatel1220@gmail.com>
2021-10-09 00:47:46 -0500

    Update index.js

--- goal_tree/frontend/src/index.js
+++ goal_tree/frontend/src/index.js
@@ -1,3 +1,2 @@
 import App from "./components/App";
-
-app.listen(process.env.PORT || 3000, () => { console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); });
+App.listen(process.env.PORT || 3000, () => { console.log("Express server listening on port %d in %s mode", this.address().port, App.settings.env); });
